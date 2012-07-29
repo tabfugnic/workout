0be55a02d635c0f19d17b57817e6824727dae912
Eric Collins <tabfugnic@gmail.com>
2012-07-29 18:25:03 -0400

    has_many through exercise muscles

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,3 +1,4 @@
 class Exercise < ActiveRecord::Base
   attr_accessible :name, :type
+  has_many :muscles, :through => :exercise_muscles
 end
--- app/models/muscle.rb
+++ app/models/muscle.rb
@@ -1,3 +1,5 @@
 class Muscle < ActiveRecord::Base
   attr_accessible :latin_name, :name
+  has_many :exercises, :through => :exercise_muscles
+  belongs_to :muscle_groups
 end
