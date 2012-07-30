958f5d6fc069dfb4dcfd09e907a03855012f685f
Eric Collins <tabfugnic@gmail.com>
2012-07-29 20:16:48 -0400

    Associations with other items for factory

--- spec/factories/exercise_muscles.rb
+++ spec/factories/exercise_muscles.rb
@@ -1,7 +1,9 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :exercise_muscle, :class => 'ExerciseMuscles' do
-    value 1
+  factory :exercise_muscle, :class => 'ExerciseMuscle' do
+    muscle {|a| a.association(:muscle)}
+    exercise {|a| a.association(:exercise)
+    value 1 + rand(10)
   end
 end
