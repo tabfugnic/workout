943547128e7d5965d044604f433fed3d7a8a5cdd
Eric Collins <tabfugnic@gmail.com>
2012-07-31 00:18:57 -0400

    Removed the anaerobic and aerobic has_one because it doesn't make sense

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -2,10 +2,6 @@ class Exercise < ActiveRecord::Base
   attr_accessible :name, :type
   has_many :muscles, :through => :exercise_muscles
   
-  #One to one relationship with aerobic and anaerobic, just so it can prep for possible polymorphism
-  has_one :anaerobic
-  has_one :aerobic
-  
   #Require field name and uniqueness
   validates :name, :presence => true, :uniqueness => true
   validates :type, :presence => true
