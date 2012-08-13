7a84a32a503c26dfbc938a716902558758e1954d
Eric Collins <tabfugnic@gmail.com>
2012-08-13 00:51:35 -0400

    Setup activities factory

--- spec/factories/activities.rb
+++ spec/factories/activities.rb
@@ -2,6 +2,19 @@
 
 FactoryGirl.define do
   factory :activity, :class => 'Activities' do
-    type ""
+    exercise
+
+    factory :aerobic do
+      type "Aerobic"
+      time rand(10000)
+      distance rand(10000)
+      speed rand(1000)
+    end
+
+    factory :anaerobic do
+      type "Anaerobic"
+      sets { 1 + rand(20) }
+      repititons { 1 + rand(5) }
+    end
   end
 end
