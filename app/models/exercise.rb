4411b9863a3c2ebf72dbdcb4fd83b2aa561b33b7
Eric Collins <tabfugnic@gmail.com>
2012-09-02 01:05:29 -0400

    Don't require type.  I didn't realize it was something used by ActiveRecord

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -6,7 +6,6 @@ class Exercise < ActiveRecord::Base
   
   #Require field name and uniqueness
   validates :name, :presence => true, :uniqueness => true
-  validates :type, :presence => true
 
   #Primary Muscles
   def primary_muscles
